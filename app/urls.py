3fe750c9990d32066ff61ab8f11a6b99744c8467
ARAMOS <aldoramosaro@gmail.com>
2022-12-21 16:16:28 -0500

    normalizaccion de tablas

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('', views.crud, name='crud')
+    path('', views.crud, name='crud'),
+    path('proveedor/', views.form_busqueda, name="form_busqueda")
 ]
--- app/views.py
+++ app/views.py
@@ -5,15 +5,23 @@ from .models import *
 # Create your views here.
 def crud(request):
     proveedores_list = Proveedores.objects.all()
+    provincia_lista = Provincias.objects.all()
+    print(provincia_lista)
     context = {
-        'proveedores_list': proveedores_list
+        'proveedores_list': proveedores_list,
+        'provincia_lista': provincia_lista
     }
     return render(request,"estructura_crud.html", context)
 
     
 
-def busqueda(request, response):
-    if(request.GET["prd"]):
-        return render(request, 'estructura_crud.html') 
-    return Httpresponse('<h1>fallo</h1>')   
+def form_busqueda(request):
+
+    provincia_lista = Provincias.objects.all()
+
+    context = {
+        'provincia_lista': provincia_lista
+    }
+
+    return render(request,"formulario_busqueda.html", context)  
 
