23c25e6b0bd80bf65ffcdbf0852e77564fe4da0e
ARAMOS <aldoramosaro@gmail.com>
2022-12-21 13:27:10 -0500

    prueba de bot

--- app/views.py
+++ app/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def crud(response):
+    
+    return render(response, 'estructura_crud.html')
+
+
+def Busqueda(response):
+
     return render(response, 'estructura_crud.html')
