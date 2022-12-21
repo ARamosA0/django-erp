92e5c342c1f6f828464f5ea5251399fe14edd0ff
Juan-E-M <juan.escobar.m@tecsup.edu.pe>
2022-12-21 12:22:05 -0500

    esquema formulario de busqueda para proveedores

--- app/views.py
+++ app/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def buscar_proveedor(request):
-    return render(request, 'proveedores/formulario_busqueda.html')
+    return render(request, 'formulario_busqueda.html')
