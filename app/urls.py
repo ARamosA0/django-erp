b2ebefccdda0fdde0e705e65689896a6cf0265ce
ARAMOS <aldoramosaro@gmail.com>
2022-12-22 08:36:35 -0500

    merge pull

--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns =[
     path('', views.crud, name='crud'),
-    path('proveedor/', views.form_busqueda, name="form_busqueda")
+    path('proveedor/', views.form_busqueda, name="form_busqueda"),
     path('insertarproveedor', views.insertar_proveedor)
 ]
