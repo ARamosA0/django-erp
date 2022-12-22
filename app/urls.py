c7d6a42719c17589ea16e68ec713f261b3e0a185
Aldo Ramos <aldoramosaro@gmail.com>
2022-12-22 12:26:54 -0500

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -6,7 +6,7 @@ urlpatterns =[
     # Pagina Principal
     path('', TemplateView.as_view(template_name="Inicio/inicio.html")),
     #Pag Provedores
-    path('prov/', proveedores, name="prov")
+    path('prov/', proveedores, name="prov"),
     #Pag Insertar Proveedores
-    path('insertarproveedor', views.insertar_proveedor)
+    #path('insertarproveedor', views.insertar_proveedor)
 ]
