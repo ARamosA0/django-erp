346aed0801ae550478fc7d2c5312d414ebd37784
Mayerlyn Taype <mayerlyn.taype@tecsup.edu.pe>
2022-12-22 08:18:26 -0500

    vista mejorada1

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('', views.crud, name='crud'),
-    path('principal/', views.index, name='index')
+    path('crud/', views.crud, name='crud'),
+    path('', views.index, name='index')
 ]
