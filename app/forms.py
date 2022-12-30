228180aaeb9b71967c9d5b1789b1781e3082ae2b
Juan-E-M <juan.escobar.m@tecsup.edu.pe>
2022-12-30 10:45:50 -0500

    funcionalidad del buscador de proveedores

--- app/forms.py
+++ app/forms.py
@@ -1,22 +1,35 @@
 from django import forms
-from django.forms import ModelForm
+from django.forms import ModelForm, Form
 from .models import *
 
 
+class ProveedorBusqueda(Form):
+    codigo = forms.CharField(label='CODIGO:',
+        widget=forms.TextInput(attrs={'class':'form-control','id':'codigo'}),required=False)
+    dni = forms.CharField(label='DNI:',
+        widget=forms.TextInput(attrs={'class':'form-control','id':'dni'}),required=False)
+    nombre = forms.CharField(label='NOMBRE:',
+        widget=forms.TextInput(attrs={'class':'form-control','id':'nombre'}),required=False)
+    telefono = forms.CharField(label='TELEFONO:',
+        widget=forms.TextInput(attrs={'class':'form-control','id':'telefono'}),required=False)
+    provincia = forms.ModelChoiceField(label='PROVINCIA:',queryset=Provincias.objects.all(),
+        widget=forms.Select(attrs={'class':'form-control','id':'provincia'}),required=False)
+    localidad = forms.CharField(label='LOCALIDAD:',
+        widget=forms.TextInput(attrs={'class':'form-control','id':'localidad'}),required=False)
 
-class ProveedorBusqueda(ModelForm):
-    class Meta:
-        model = Persona
-        # fields = '__all__'
-        fields = ('id', 'dni', 'nombre', 'telefono', 'codprovincia', 'localidad')
-        widgets = {
-            'codigo': forms.TextInput(attrs={'class': 'form-control'}),
-            'dni': forms.TextInput(attrs={'class': 'form-control'}),
-            'nombre': forms.TextInput(attrs={'class': 'form-control'}),
-            'telefono': forms.TextInput(attrs={'class': 'form-control'}),
-            'codprovincia': forms.Select(attrs={'class': 'form-control'}),
-            'localidad': forms.TextInput(attrs={'class': 'form-control'})
-        }
+# class ProveedorBusqueda(ModelForm):
+#     class Meta:
+#         model = Persona
+#         # fields = '__all__'
+#         fields = ('id', 'dni', 'nombre', 'telefono', 'codprovincia', 'localidad')
+#         widgets = {
+#             'codigo': forms.TextInput(attrs={'class': 'form-control'}),
+#             'dni': forms.TextInput(attrs={'class': 'form-control'}),
+#             'nombre': forms.TextInput(attrs={'class': 'form-control'}),
+#             'telefono': forms.TextInput(attrs={'class': 'form-control'}),
+#             'codprovincia': forms.Select(attrs={'class': 'form-control'}),
+#             'localidad': forms.TextInput(attrs={'class': 'form-control'})
+#         }
 
 
 
