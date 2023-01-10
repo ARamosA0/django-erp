1c9ec331331e973c162b15865b24c68ba5169d6b
Anthonella Rua <anthonella.rua@tecsup.edu.pe>
2023-01-10 14:10:38 -0500

    crud familias

--- app/views.py
+++ app/views.py
@@ -221,16 +221,62 @@ def familias(request):
         'familias_list': familias_list,
         'busquedaform': busquedaform
     }
+    if request.method == 'POST':
+        busquedaform = FamiliaBusqueda(request.POST)
+        if busquedaform.is_valid():
+            data = Familia.objects.filter(borrado='0')
+            data = data.filter(pk=busquedaform.cleaned_data['codigo'])  if busquedaform.cleaned_data['codigo'] else data
+            data = data.filter(nombre=busquedaform.cleaned_data['nombre'])  if busquedaform.cleaned_data['nombre'] else data
+            context['familias_list']=data
+            context['busquedaform']=busquedaform
+    else:
+        busquedaform = FamiliaBusqueda()
     return render(request, "Familias/estructura_crud_fam.html",context)
 
 def agregar_familia(request):
-    return
+    enviado = False
+    if request.method == 'POST':
+        in_familia_per = AgregarFamilia(request.POST)
+        if in_familia_per.is_valid():
+            in_familia_per.save()
+            return HttpResponseRedirect('agregarfam?enviado=True')
 
-def ver_familia(request):
-    return
+    else:
+        in_familia_per= AgregarFamilia()
+        if 'enviado' in request.GET:
+            enviado = True
+    context = {
+        'in_familia_per':in_familia_per,
+        'enviado':enviado, 
+    }
+    return render(request, "Familias/formulario_insertar_familia.html", context)
 
-def editar_familia(request):
-    return
+def ver_familia(request, id):
+    familia_list = Familia.objects.get(id=id)
+    context = {
+        'fam': familia_list
+    }
+    return render(request, "Familias/familia.html", context)
 
-def eliminar_familia(request):
-    return
+def editar_familia(request, id):
+    familia_put = Familia.objects.get(id=id)
+    in_familia_per = AgregarFamilia(request.POST or None, instance=familia_put)
+    if in_familia_per.is_valid():
+            in_familia_per.save()       
+            return redirect('fam')
+    
+    context = {
+        'in_familia_per':in_familia_per,
+    }    
+    return render(request, "Familias/formulario_insertar_familia.html", context)
+
+def eliminar_familia(request,id):
+    enviado = False
+    del_familia = Familia.objects.filter(id=id)
+    if request.method =="POST":
+        del_familia.delete()
+        return HttpResponseRedirect('?enviado=True')
+    context = {
+        'enviado':enviado
+    }
+    return render(request, "Familias/delete_familia.html", context)
