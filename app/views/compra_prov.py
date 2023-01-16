cf4d19ea70faa8c802d2e6294104790d55ee08a4
ARAMOS <aldoramosaro@gmail.com>
2023-01-16 12:19:04 -0500

    avance compra

--- app/views/compra_prov.py
+++ app/views/compra_prov.py
@@ -16,4 +16,42 @@ def agregar_compra_prov(request):
 
     context={}
 
+    if request == 'POST':
+        valid_prov_form = request.POST.get("valid_prov_form",None)
+        validfac_finalform = request.POST.get("validfac_finalform", None)
+
+
+        # if eliminar_art_venta:
+        #     del_flc = Factura_linea_clie.objects.filter(pk=request.POST['eliminar_art_venta'])
+        #     del_flc.delete()
+        #     suma_importes(context)
+ 
+        
+        if valid_prov_form:
+            nomarticulo = request.POST["nomarticulo"] 
+            articulo_data = Articulos.objects.get(referencia=nomarticulo)
+            compra_prov_last = Compra_prov.objects.last()
+            compra_prov_last_id = compra_prov_last.factura.id
+            compra_linea_prov = Compra_linea_prov()
+            compra_linea_prov.factura_cliente = Factura_clie.objects.last()
+            compra_linea_prov.codproducto = Articulos.objects.get(referencia=nomarticulo)
+            compra_linea_prov.precio = articulo_data.precio_compra
+            cantidad = request.POST["cantidad_art"]
+            compra_linea_prov.cantidad = cantidad
+            descuento = request.POST["descuento_art"]
+            compra_linea_prov.dsctoproducto = descuento
+            compra_linea_prov.importe = (articulo_data.precio_compra * int(cantidad)) - float(descuento)
+            compra_linea_prov.save()    
+            # suma_importes(context)
+
+        #Modifica Factura
+        if validfac_finalform:
+            factura_put = Factura.objects.last()
+            factura_put.totalfactura = request.POST["total_fac"]
+            factura_put.save()
+            fac_clie_last = Factura_clie.objects.last()
+            fac_clie_last_id = fac_clie_last.factura.id
+            art_fac = Factura_linea_clie.objects.filter(factura_cliente_id = fac_clie_last_id)
+            context['articulo_factura'] = art_fac
+
     return render(request, "CompraProv/agregar_fac_prov.html", context)
