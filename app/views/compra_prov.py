fd44f61fcc145dee9c0e656f2c51799627313267
ARAMOS <aldoramosaro@gmail.com>
2023-01-16 14:01:32 -0500

    provincias

--- app/views/compra_prov.py
+++ app/views/compra_prov.py
@@ -4,6 +4,18 @@ from django.shortcuts import redirect
 from app.models import *
 from app.forms import *
 
+def suma_importes(context):
+    art_fac = Factura_linea_clie.objects.filter(factura_cliente_id = Factura_clie.objects.last().pk)
+    suma_importes = 0
+    for i in art_fac:
+        suma_importes += i.importe
+    factura_last = Factura.objects.last()
+    total_iva = suma_importes * (factura_last.iva/100) 
+    total_fac = suma_importes + total_iva
+    context['articulo_factura'] = art_fac
+    context['suma_importe'] = suma_importes
+    context['total_fac'] = total_fac
+
 def define_context(context={},**kwargs):
     return {**context,**kwargs}
 def compra_prov(request):
@@ -22,6 +34,7 @@ def agregar_compra_prov(request):
     if request.method == 'POST':
         nueva_factura = NuevaFactura(request.POST)
         ruc_prov = request.POST.get('ruc_prov',None)
+        eliminar_art_compra = request.POST.get("eliminar_art_compra",None)
         prov = Proveedores.objects.filter(ruc=ruc_prov)
         if ruc_prov!='' and nueva_factura.is_valid():
             nombre_prov = prov[0] if prov else "RUC INVÁLIDO"
@@ -35,10 +48,10 @@ def agregar_compra_prov(request):
         validfac_finalform = request.POST.get("validfac_finalform", None)
 
 
-        # if eliminar_art_venta:
-        #     del_flc = Factura_linea_clie.objects.filter(pk=request.POST['eliminar_art_venta'])
-        #     del_flc.delete()
-        #     suma_importes(context)
+        if eliminar_art_compra:
+            del_flc = Factura_linea_clie.objects.filter(pk=request.POST['eliminar_art_compra'])
+            del_flc.delete()
+            suma_importes(context)
  
         
         if valid_prov_form:
@@ -47,16 +60,14 @@ def agregar_compra_prov(request):
             compra_prov_last = Compra_prov.objects.last()
             compra_prov_last_id = compra_prov_last.factura.id
             compra_linea_prov = Compra_linea_prov()
-            compra_linea_prov.factura_cliente = Factura_clie.objects.last()
+            compra_linea_prov.compra_cliente = Compra_prov.objects.last()
             compra_linea_prov.codproducto = Articulos.objects.get(referencia=nomarticulo)
             compra_linea_prov.precio = articulo_data.precio_compra
             cantidad = request.POST["cantidad_art"]
             compra_linea_prov.cantidad = cantidad
-            descuento = request.POST["descuento_art"]
-            compra_linea_prov.dsctoproducto = descuento
-            compra_linea_prov.importe = (articulo_data.precio_compra * int(cantidad)) - float(descuento)
+            compra_linea_prov.importe = (articulo_data.precio_compra * int(cantidad))
             compra_linea_prov.save()    
-            # suma_importes(context)
+            suma_importes(context)
 
         #Modifica Factura
         if validfac_finalform:
