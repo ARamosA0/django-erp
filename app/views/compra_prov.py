5b2bf110b2887ca0b90beb482bfad1e8c0cf6e5c
Juan-E-M <juan.escobar.m@tecsup.edu.pe>
2023-01-16 12:26:37 -0500

    avance compras

--- app/views/compra_prov.py
+++ app/views/compra_prov.py
@@ -4,6 +4,8 @@ from django.shortcuts import redirect
 from app.models import *
 from app.forms import *
 
+def define_context(context={},**kwargs):
+    return {**context,**kwargs}
 def compra_prov(request):
 
     context = {
@@ -14,6 +16,18 @@ def compra_prov(request):
 
 def agregar_compra_prov(request):
 
-    context={}
+
+    context={'nueva_factura_form':NuevaFactura()}
+
+    if request.method == 'POST':
+        nueva_factura = NuevaFactura(request.POST)
+        ruc_prov = request.POST.get('ruc_prov',None)
+        prov = Proveedores.objects.filter(ruc=ruc_prov)
+        if ruc_prov!='' and nueva_factura.is_valid():
+            nombre_prov = prov[0] if prov else "RUC INVÁLIDO"
+            context = define_context(context,ruc_prov=ruc_prov,
+                                    nombre_prov=nombre_prov,nueva_factura_form=nueva_factura)
+
+
 
     return render(request, "CompraProv/agregar_fac_prov.html", context)
